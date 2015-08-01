f9425fe436ff0f873243cc4ae6b5974f95e0eed1
Stijn Buys <ingar@osirion.org>
2015-08-02 00:55:01 +0200

    Added image crunching support to the pk3 packaging scripts.

--- pk3-tex.sh
+++ pk3-tex.sh
@@ -22,10 +22,69 @@ echo "shared_vega" > release/tex/scripts/shaderlist.txt
 
 # textures/
 cp -r textures/shared_vega_src/ release/tex/textures/
-# TODO crunch png files
 
-# pk3
+# enter output directory
 cd release/tex
+
+# compress textures
+cd textures/shared_vega_src/
+
+CONVERT=`which convert`
+if [ ! -z "${CONVERT}" ]; then
+
+	# convert preview textures to JPEG
+	for file in *_p.png; do
+		if [ -f ${file} ]; then
+			echo "Converting ${file%.png}.jpg ..."
+			convert -quality 80 ${file} ${file%.png}.jpg && rm ${file}
+		fi
+	done
+else
+	echo "WARNING convert not found"
+fi
+
+CRUNCH=`which crunch`
+if [ ! -z "${CRUNCH}" ]
+then
+
+	# convert normal maps
+	for file in *_normal.png
+	do
+		if [ -f ${file} ]
+		then
+			echo "Crunching ${file%.png}.crn ..."
+			convert -alpha deactivate ${file} ${file%.png}.tga
+			${CRUNCH} -quality 255 -dxn -renormalize ${file%.png}.tga -out ${file%.png}.crn && rm ${file}
+			rm ${file%.png}.tga
+		fi
+	done
+		
+	# convert blend maps
+	for file in *_blend.png
+	do
+		if [ -f ${file} ]
+		then
+			echo "Crunching ${file%.png}.crn ..."
+			${CRUNCH} -quality 255 ${file%} -out ${file%.png}.crn && rm ${file}
+		fi
+	done
+	
+	# convert other image maps
+	for file in *_diffuse.png *_glow.png *_specular.png
+	do
+		if [ -f ${file} ]
+		then
+			echo "Crunching ${file%.png}.crn ..."
+			convert -alpha deactivate ${file} ${file%.png}.tga
+			${CRUNCH} -quality 255 ${file%.png}.tga -out ${file%.png}.crn && rm ${file}
+			rm ${file%.png}.tga
+		fi
+	done
+else
+	echo "WARNING crunch not found"
+fi
+
+cd ../..
 
 TIMESTAMP=`date +"%Y%d%d%H%M"`
 zip -r "../tex-vega_t${TIMESTAMP}.pk3" *
